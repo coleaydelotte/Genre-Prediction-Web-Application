58c55c034b074daef2cd2e0b2614272973c12e15
Cole Aydelottte <cole.aydelotte@gmail.com>
2025-01-08 14:02:14 -0500

    Fixed Sign up sheet.
    Fixed Sign up sheet.

--- src/pages/Sign-Up/sign-up.jsx
+++ src/pages/Sign-Up/sign-up.jsx
@@ -2,6 +2,21 @@ function SignUp() {
     return (
         <div>
             <h1>Sign Up</h1>
+            <form>
+                <label>
+                    Email
+                    <input type="email" />
+                </label>
+                <label>
+                    Password
+                    <input type="password" />
+                </label>
+                <label>
+                    Confirm Password
+                    <input type="password" />
+                </label>
+                <button>Sign Up</button>
+            </form>
         </div>
     )
 }
