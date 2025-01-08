5c990f3a48978096e35d2de04373b53e8536795f
Cole Aydelottte <cole.aydelotte@gmail.com>
2025-01-07 20:08:51 -0500

    Update sign-in.jsx.
    Update sign-in.jsx.

--- src/pages/Sign-In/sign-in.jsx
+++ src/pages/Sign-In/sign-in.jsx
@@ -2,6 +2,17 @@ function SignIn () {
     return (
         <div>
             <h1>Sign In</h1>
+            <form>
+                <label>
+                    Email
+                    <input type="email" />
+                </label>
+                <label>
+                    Password
+                    <input type="password" />
+                </label>
+                <button>Sign In</button>
+            </form>
         </div>
     )
 }
