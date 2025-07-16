2841459324edb5c4406da3789274ff1bcb66631b
Cole Aydelottte <cole.aydelotte@gmail.com>
2025-07-16 04:11:10 -0400

    Fixed modal error we were getting.
    Fixed modal error we were getting. :)

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -15,10 +15,14 @@ function Home() {
   return (
     <div className="home">
       {modalState && (
-        <Modal setModalState={setModalState} description={ "This is a modal" } components={components} cName={"modal"} />
+        <Modal description={ "This is a modal" } components={components} cName={"modal"} />
       )}
       <div className="flex-content">
-        <button className="button" onClick={() => {setModalState(!modalState)}}> Open Modal </button>
+        <button className="button" onClick={() => {
+          setModalState(!modalState)
+        }}>
+          Open Modal
+        </button>
         <img src="https://cdn-icons-png.flaticon.com/512/0/532.png" alt="Upload" className="upload"
           onClick={() => {
             Upload();
@@ -28,5 +32,6 @@ function Home() {
     </div>
   );
 }
-
+// After the file is uploaded, we will need to convert it to a mel spectogram for prediction.
+// We will probably use a library like librosa for this.
 export default Home;
