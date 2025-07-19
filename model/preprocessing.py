9a91352a0ab9ce45608947fc8813ca999cea1226
Cole Aydelottte <cole.aydelotte@gmail.com>
2025-07-19 04:20:20 -0400

    Updated function to convert the file into MEL spectrograms.
    Updated function to convert the file into MEL spectrograms. :)

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -1,7 +1,12 @@
 from pydub import AudioSegment
 from pydub.silence import make_chunks
+import librosa
 
-def split_audio_file(file_path, chunk_length=3000):
+def preprocessing_into_spectograms(file_path, chunk_length=3000):
     audio = AudioSegment.from_file(file_path)
     chunks = make_chunks(audio, chunk_length)
-    return chunks
+    spectograms = []
+    for _, chunk in enumerate(chunks):
+        mel_spectrogram = librosa.feature.melspectrogram(y=chunk.get_array_of_samples(), sr=chunk.frame_rate)
+        spectograms.append(mel_spectrogram)
+    return spectograms
