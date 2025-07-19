b6bd86062f23dda9960cc42b88ff1d6102a61557
Cole Aydelottte <cole.aydelotte@gmail.com>
2025-07-19 04:20:55 -0400

    Fixing typo.
    Fixing typo. :)

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -5,8 +5,8 @@ import librosa
 def preprocessing_into_spectograms(file_path, chunk_length=3000):
     audio = AudioSegment.from_file(file_path)
     chunks = make_chunks(audio, chunk_length)
-    spectograms = []
+    spectrograms = []
     for _, chunk in enumerate(chunks):
         mel_spectrogram = librosa.feature.melspectrogram(y=chunk.get_array_of_samples(), sr=chunk.frame_rate)
-        spectograms.append(mel_spectrogram)
-    return spectograms
+        spectrograms.append(mel_spectrogram)
+    return spectrograms
