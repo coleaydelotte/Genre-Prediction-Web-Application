00064af32375fc7a3b2d9146913dab21f0fc9f3a
Cole Aydelottte <cole.aydelotte@gmail.com>
2025-08-11 20:48:23 -0400

    Updated the up script.
    Updated the up script. :)

--- model/create.sh
+++ model/create.sh
@@ -1,2 +1,8 @@
 sudo mkdir -p ./uploads
 sudo mkdir -p ./data
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r ./model/requirements.txt
+chmod +x ./model/upload_cleaner_script.sh
+./model/upload_cleaner_script.sh & python3 ./model/api.py
